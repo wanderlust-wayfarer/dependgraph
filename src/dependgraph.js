eb8aba6773e10eb8e010238ea0567050e079cb46
Michael Pay <wanderlust.wayfarer@icloud.com>
2023-05-01 00:27:35 -0700

    scaffolding for analyzing dependencies

--- src/dependgraph.js
+++ src/dependgraph.js
@@ -1,34 +1,58 @@
-const fs = require('fs');
+const fs = require("fs-extra");
+const path = require("path");
 
 class DependGraph {
-  constructor() {
+  constructor(root) {
     this.nodes = new Map();
+    this.breadthFirstTraversal(root);
   }
 
-  addNode(value) {
-    this.nodes.set(value, []);
+  // nodes represent files
+  addNode(path) {
+    this.nodes.set(path, {
+      imports: [],
+      exports: [],
+    });
   }
 
-  addEdge(node1, node2) {
-    this.nodes.get(node1).push(node2);
-    this.nodes.get(node2).push(node1);
+  // edges represent dependencies
+  addEdge(parent, child) {
+    this.nodes.get(child).imports.push(parent);
+    this.nodes.get(parent).exports.push(child);
+  }
+
+  analyzeAndAddDependencies(node) {
+    this.addNode(node);
+    fs.readFile(node, "utf8")
+      .then((fileContent) => {
+        // read all imports and requires
+        // for each, add edges
+      })
+      .catch((err) =>
+        console.error(
+          `DependGraph.analyzeAndAddDependencies: Unable to read file contents.\n  ${err}`
+        )
+      );
   }
 
   breadthFirstTraversal(startNode) {
-    const visited = new Set();
     const queue = [startNode];
+    const visited = new Set();
 
     while (queue.length > 0) {
       const currentNode = queue.shift();
+      const contents = fs.readdirSync(currentNode);
 
-      if (!visited.has(currentNode)) {
-        visited.add(currentNode);
-        console.log(currentNode);
-
-        for (const neighbor of this.nodes.get(currentNode)) {
-          queue.push(neighbor);
+      if (fs.statSync(currentNode).isDirectory()) {
+        for (const file of contents) {
+          const filepath = path.join(currentNode, file);
+          queue.push(filepath);
         }
+      } else {
+        this.analyzeAndAddDependencies(currentNode);
       }
+
+      visited.add(currentNode);
     }
   }
-};
+}
